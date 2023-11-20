1ba943befadae4a291b62894bb26cd802a66cb51
math92n4 <math92n4@stud.kea.dk>
2023-11-20 12:39:26 +0100

    first commit

---
+++ src/main/java/com/example/bilbackend/BilBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.bilbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bilbackend/BilBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bilbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
