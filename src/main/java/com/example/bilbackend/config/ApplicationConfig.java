1dfe688c60e4030c93891e70e371255daa1d6239
math92n4 <math92n4@stud.kea.dk>
2023-11-23 08:24:55 +0100

    spring security authorization

--- src/main/java/com/example/bilbackend/config/ApplicationConfig.java
+++ src/main/java/com/example/bilbackend/config/ApplicationConfig.java
@@ -2,6 +2,7 @@ package com.example.bilbackend.config;
 
 import com.example.bilbackend.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
